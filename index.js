4cc9d6b65866ac4764f5606a4d04a5ed2d548683
Alexander Rulkens <rulkens@gmail.com>
2015-07-27 23:49:48 +0200

    cleaning up

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-var Eos = require('./lib/eos/client/eos'),
+var Eos = require('./lib/eos/api'),
+    _ = require('lodash'),
+    Light = require('./lib/eos/light');
     defaultProgram = require('./lib/eos/default-program');
 
-eos = new Eos();
+var eos = new Eos();
+var scheduler;
 
-eos.on('connect', function () {
-    defaultProgram(eos);
+eos.connect(function() {
+    eos.allOff();
+    //defaultProgram(eos);
+    console.log('connected!');
+
+    // start the program in
+    setTimeout(function(){
+        scheduler = defaultProgram(eos);
+    }, 2000);
 });
 
+// this should not happen, yet there will be some moment it inadvertently does
+eos.on('error', function (err) {
+    console.warn('This is impossible! an ERROR occurred', err);
+});
+
+// handle exits gracefully
 process.on('SIGINT', function(){
     console.log('Turning off all lights');
-    eos.api('off');
-    process.exit();
+
+    // turn off scheduler
+    if(scheduler) scheduler.stop();
+    eos.allOff();
+
+    setTimeout(function(){
+        eos.disconnect();
+        process.exit();
+    }, 0);
+
 });
