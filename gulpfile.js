ec39846ac9eda88c43c33996e746d71dc671a5a1
Alexander Rulkens <rulkens@gmail.com>
2015-09-16 02:18:35 +0200

    adding color to the mix

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 /**
  * Created by rulkens on 08/09/15.
  */
+
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('run', function() {
+    nodemon({
+        script : 'index.js'
+        , ext  : 'js html'
+        , env  : {'NODE_ENV' : 'development'}
+    });
+});
---
+++ test.js
@@ -0,0 +1,5 @@
+var Color = require('color');
+
+var c = Color('0xFF0000');
+
+console.log('c', c);
