37ee7abd9c88e4fc31a2fc364d74df2856f0a3c0
Alexander Rulkens <rulkens@gmail.com>
2015-11-24 10:18:21 +0100

    some tests

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
-var Color = require('color');
+var Color     = require('color'),
+    colorUtil = require('./lib/eos/util.color');
 
-var c = Color('0xFF0000');
+//var red = colorUtil.intToColor(0xFF0000),
+//    green = colorUtil.intToColor(0xFF00),
+//    blue = colorUtil.intToColor(0xFF);
 
-console.log('c', c);
+/*var red = colorUtil.intToRgb(0xFF0000),
+ green = colorUtil.intToRgb(0xFF00),
+ blue = colorUtil.intToRgb(0xFF);*/
+
+var red   = colorUtil.rgbToInt([0xFF, 0, 0]),
+    green = colorUtil.rgbToInt([0, 0xFF, 0]),
+    blue  = colorUtil.rgbToInt([0, 0, 0xFF]);
+
+console.log('red, green, blue', red, green, blue);
